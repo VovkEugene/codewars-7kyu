02cc7b15c4276f195a27688bef56fe63f39f6336
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-02-23 23:22:50 +0300

    add task: Find the divisors!

---
+++ FindDivisors.js
@@ -0,0 +1,37 @@
+/**
+ * Найдите делители!
+ *
+ * Создайте функцию с именем divisors, которая принимает целое число n > 1 и
+ * возвращает массив со всеми делителями целого числа (за исключением 1 и самого числа),
+ * от наименьшего до наибольшего.
+ * Если число простое, верните строку '(integer) is prime'
+ *
+ * Example:
+ * divisors(12); // should return [2,3,4,6]
+ * divisors(25); // should return [5]
+ * divisors(13); // should return "13 is prime"
+ */
+
+function divisors(integer) {
+  let array = [];
+
+  for (let i = 2; i < integer; i++) {
+    if (integer % i == 0) {
+      array.push(i);
+    }
+  }
+
+  return array.length == 0 ? `${integer} is prime` : array;
+}
+
+function isPrime(integer) {
+  for (let i = 2; i < integer; i++) {
+    if (integer % i == 0) {
+      return false;
+    }
+  }
+
+  return integer > 0;
+}
+
+console.log(divisors(13));
