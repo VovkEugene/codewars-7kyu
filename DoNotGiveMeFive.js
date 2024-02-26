2430b573fe6f0bdc8503f0d663414f991ce0c74c
Eugene Vovk <evgeniyvovk2009@yandex.ru>
2024-02-26 14:17:05 +0300

    add task: Don't give me five!

---
+++ DoNotGiveMeFive.js
@@ -0,0 +1,31 @@
+/**
+ * Не ставь мне пять!
+ *
+ * В этом ката вы получаете начальный номер и конечный номер региона и
+ * должны вернуть количество всех чисел, кроме чисел с 5 в нем.
+ * Начальный и конечный номера указаны включительно!
+ *
+ * Examples:
+ * 1,9 -> 1,2,3,4,6,7,8,9 -> Result 8
+ * 4,17 -> 4,6,7,8,9,10,11,12,13,14,16,17 -> Result 12
+ */
+
+function dontGiveMeFive(start, end) {
+  let count = 0;
+  let str = "";
+
+  for (let i = start; i <= end; i++) {
+    str = i.toString();
+
+    if (str.includes("5")) {
+      continue;
+    }
+
+    count++;
+  }
+
+  return count;
+}
+
+console.log(dontGiveMeFive(1, 9));
+console.log(dontGiveMeFive(4, 17));
